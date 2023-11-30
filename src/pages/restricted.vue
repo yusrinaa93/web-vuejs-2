25beca6a145079092da39e8d055edf44928d52e7
yusrinaa93 <yusrinamastura93@gmail.com>
2023-12-01 00:03:26 +0800

    first commit

---
+++ src/pages/restricted.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Selamat datang di Dashboard Admin. Halaman ini hanya dapat diakses oleh administrator untuk mengelola pengaturan dan informasi penting.
+  </div>
+</template>
